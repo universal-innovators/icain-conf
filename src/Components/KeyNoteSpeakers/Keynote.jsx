1a0d86e2ff4d2f1a3c04d9aa6a4c91190846c02f
Paras Jain <64364245+ParasJain013@users.noreply.github.com>
2024-08-22 21:58:53 +0530

    fix

--- src/Components/KeyNoteSpeakers/Keynote.jsx
+++ src/Components/KeyNoteSpeakers/Keynote.jsx
@@ -6,16 +6,16 @@ export default function Keynote() {
             <h1 className='keynoteHeading'>Keynote Speakers</h1>
             <div className="keynoteSpeaker">
                 <div className="keynoteCard">
+                    <img src="aninda.jpeg" alt="" srcset="" />
+                    <p> <strong>Mr. Aninda Bose</strong>
+                        <br /> Executive Editor Interdisciplinary Applied Sciences, Computational Intelligence, Energy             Springer Nature London</p>
+                </div>
+                <div className="keynoteCard">
                     <img src="biswajit.jpg" alt="" srcset="" />
                     <p> <strong>Dr. Biswajit Brahma </strong>
                         <br />Data Expert, McKesson Corporation, USA.
                         Over 40 Publications in SCI/Scopus journals.
                         10+ patents and copyrights </p>
-                </div>
-                <div className="keynoteCard">
-                    <img src="aninda.jpeg" alt="" srcset="" />
-                    <p> <strong>Mr. Aninda Bose</strong>
-                        <br /> Executive Editor Interdisciplinary Applied Sciences, Computational Intelligence, Energy             Springer Nature London</p>
                 </div>
             </div>
         </>
