6767dfd6514ecd3264a76430e867529c20158b35
Paras Jain <64364245+ParasJain013@users.noreply.github.com>
2024-09-05 19:47:58 +0530

    downloads added

--- src/Components/Downloads/Download.jsx
+++ src/Components/Downloads/Download.jsx
@@ -6,6 +6,10 @@ import Pdf1 from "./pdfs/Manuscript.pdf";
 import Pdf2 from "./pdfs/Checklist.pdf";
 import Pdf3 from "./pdfs/SESSION PROPOSAL.docx";
 import Pdf4 from "./pdfs/WORKHOP PROPOSAL.docx";
+import BACKGROUND_POSTER_ICAIN from "./pdfs/BACKGROUND POSTER ICAIN.jpg" 
+// import springer_ppt from "./pdfs/springer_ppt.pptx"
+import ICAIN_2024_PPT_Template_ELSEVIER from "./pdfs/ICAIN_2024_PPT_Template- ELSEVIER.pptx"
+import ICAIN_2024_PPT_Template_SPRINGER from "./pdfs/ICAIN_2024_PPT_Template-SPRINGER.pptx"
 import Zip from "./splnproc1703s.docx";
 // import Pdf6 from "./pdfs/Elsevier SSRN_Word Template.pdf";
 import ImpNotices from "../ImpNotices/ImpNtext";
@@ -74,14 +78,32 @@ function Downloads() {
               </div>
 
               {/* 6th  */}
-              {/* <div className="pdf pdf1">
-                <a href={Pdf6} target="_blank">
-                  <h1>Elsevier SSRN Paper Template</h1>
+              <div className="pdf pdf1">
+                <a href={ICAIN_2024_PPT_Template_SPRINGER} target="_blank">
+                  <h1>ICAIN 2024 PPT Template-SPRINGER</h1>
                 </a>
-                <a href={Pdf6} target="_blank">
+                <a href={ICAIN_2024_PPT_Template_SPRINGER} target="_blank">
                   <FaDownload />
                 </a>
-              </div> */}
+              </div>
+              {/* 7th  */}
+              <div className="pdf pdf1">
+                <a href={ICAIN_2024_PPT_Template_ELSEVIER} target="_blank">
+                  <h1>ICAIN 2024 PPT Template-ELSEVIER</h1>
+                </a>
+                <a href={ICAIN_2024_PPT_Template_ELSEVIER} target="_blank">
+                  <FaDownload />
+                </a>
+              </div>
+              {/* 8th  */}
+              <div className="pdf pdf1">
+                <a href={BACKGROUND_POSTER_ICAIN} target="_blank">
+                  <h1>BACKGROUND POSTER ICAIN</h1>
+                </a>
+                <a href={BACKGROUND_POSTER_ICAIN} target="_blank">
+                  <FaDownload />
+                </a>
+              </div>
             </div>
 
             {/* impdate  */}
