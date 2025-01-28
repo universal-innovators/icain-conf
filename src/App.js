505074d837a8444ef21b7cd7144caa198e313553
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2025-01-28 23:23:23 +0530

    disable

--- src/App.js
+++ src/App.js
@@ -57,37 +57,37 @@ import { useEffect } from "react";
 
 // import Footerr from "./Components/Footer/Footerr";
 function App() {
-  // useEffect(() => {
-  //   const handleContextMenu = (e) => {
-  //     e.preventDefault();
-  //   };
+  useEffect(() => {
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+    };
 
-  //   document.addEventListener("contextmenu", handleContextMenu);
-  //   return () => {
-  //     document.removeEventListener("contextmenu", handleContextMenu);
-  //   };
-  // }, []);
-  // useEffect(() => {
-  //   const handleKeyDown = (e) => {
-  //     // Disable F12 (Developer Tools)
-  //     if (e.key === "F12") {
-  //       e.preventDefault();
-  //     }
+    document.addEventListener("contextmenu", handleContextMenu);
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      // Disable F12 (Developer Tools)
+      if (e.key === "F12") {
+        e.preventDefault();
+      }
 
-  //     // Disable Ctrl+Shift+I/J/C/U
-  //     if (
-  //       (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
-  //       (e.ctrlKey && e.key === "U")
-  //     ) {
-  //       e.preventDefault();
-  //     }
-  //   };
+      // Disable Ctrl+Shift+I/J/C/U
+      if (
+        (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
+        (e.ctrlKey && e.key === "U")
+      ) {
+        e.preventDefault();
+      }
+    };
 
-  //   window.addEventListener("keydown", handleKeyDown);
-  //   return () => {
-  //     window.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <>
       <HeaderMain />
