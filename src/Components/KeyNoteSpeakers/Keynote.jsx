6007d751707c16c423753ae0e05724f7c71a474c
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2025-05-19 15:44:53 +0530

    speakers

--- src/Components/KeyNoteSpeakers/Keynote.jsx
+++ src/Components/KeyNoteSpeakers/Keynote.jsx
@@ -16,6 +16,26 @@ export default function Keynote() {
                     <p> <strong>Dr. Korhan Cengiz</strong>
                         <br />Associate Research Professor, Department of Electrical Engineering, Prince Mohammad Bin Fahd University, KSA</p>
                 </div>
+                <div className="keynoteCard">
+                    <img src="marta.gif" alt="" srcset="" />
+                    <p> <strong>Associate professor Marta Harničárová, PhD</strong>
+                        <br />Institute of Technology and Business in České Budějovice, Faculty of Technology, Czech Republic</p>
+                </div>
+                <div className="keynoteCard">
+                    <img src="jan.gif" alt="" srcset="" />
+                    <p> <strong>professor Jan Valíček, Ph.D</strong>
+                        <br />Institute of Technology and Business in České Budějovice, Faculty of Technology, Czech Republic</p>
+                </div>
+                <div className="keynoteCard">
+                    <img src="samiya.jpg" alt="" srcset="" />
+                    <p> <strong>Dr Samiya Khan</strong>
+                        <br /></p>
+                </div>
+                <div className="keynoteCard">
+                    <img src="kat.jpg" alt="" srcset="" />
+                    <p> <strong>Dr. Kathrin Kind</strong>
+                        <br />Member of the Board of Advisors. Swiss School of Business and Management ; Chief Data Scientist / AI A. Director Global Growth Markets (EMEA/APAC) Cognizant</p>
+                </div>
             </div>
         </>
     )
