5991ac57016c19866e7d7a6154f1ee75adfb83fd
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2026-09-21 22:59:49 +0530

    logo

--- src/Components/impDates/impDtext.jsx
+++ src/Components/impDates/impDtext.jsx
@@ -6,19 +6,19 @@ import "./ImpDtext.css";
 const arr = [
   {
     h4: "Deadline for manuscript submission: ",
-    p: "September 20th, 2026",
-  },
-  {
-    h4: "Notification of first review:",
     p: "September 30th, 2026",
   },
   {
-    h4: "Submission of revised manuscript:",
+    h4: "Notification of first review:",
     p: "October 10th, 2026",
   },
   {
-    h4: "Notification of final acceptance:",
+    h4: "Submission of revised manuscript:",
     p: "October 20th, 2026",
+  },
+  {
+    h4: "Notification of final acceptance:",
+    p: "October 25th, 2026",
   },
   {
     h4: "Final manuscript due",
